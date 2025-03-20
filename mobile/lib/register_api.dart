f083820adbceae98497ed6138489370b4389bf50
Ari-fauzi <arifauzi441@gmail.com>
2025-03-20 23:13:41 +0700

    Menambahkan fitur register serta menghubungkan dengan backend

---
+++ mobile/lib/register_api.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class RegisterApi {
+  String msg = '';
+  int statusCode = 0;
+
+  RegisterApi({required this.msg, required this.statusCode});
+
+  factory RegisterApi.createRegisterApi(Map<String, dynamic> object) {
+    return RegisterApi(msg: object['msg'], statusCode: object['statusCode']);
+  }
+
+  static Future<RegisterApi> register(email, password, alamat) async {
+    String apiURL = "http://localhost:3000/auth/register";
+    var apiResult = await http.post(Uri.parse(apiURL),
+        body: {"email": email, "password": password, "alamat": alamat});
+
+    var registerResult = json.decode(apiResult.body);
+
+    return RegisterApi.createRegisterApi(
+        {...registerResult, "statusCode": apiResult.statusCode});
+  }
+}
