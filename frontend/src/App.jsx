79f8db994aa41d4519f1824d29bc08aba089076f
Alfatih <rahmatalfatih04@gmail.com>
2025-04-28 12:00:31 +0700

    navigasi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Tokokeris from "./Pages/tokokeris";
 import Detailproduk from "./Pages/detailproduk";
+import ProdukTerlaris from "./Pages/produkterlaris";
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/toko-keris" element={<Tokokeris />} />
         <Route path="/detail-produk" element={<Detailproduk />} />
+        <Route path="/produk-terlaris" element={<ProdukTerlaris />} />
       </Routes>
     </Router>
   )
