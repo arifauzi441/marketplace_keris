a9819bc8fab13fc39896cc91afb269cb73695de1
Ari-fauzi <arifauzi441@gmail.com>
2025-04-28 22:13:00 +0700

    coba 6 deploy

--- docs/src/main.jsx
+++ docs/src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter basename="/marketplace_keris">  {/* Membungkus App */}
+    <HashRouter>  {/* Membungkus App */}
       <App />
     </HashRouter>
   </StrictMode>
