6c2a7abd7229c71fed5d6dd33dde9a93491069fe
Ari-fauzi <arifauzi441@gmail.com>
2025-04-28 21:56:03 +0700

    memperbaiki router

--- docs/src/App.jsx
+++ docs/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import "./index.css";
 import Tokokeris from "./Pages/tokokeris";
 import Detailproduk from "./Pages/detailproduk";
@@ -9,14 +9,12 @@ import ProdukTerlaris from "./Pages/produkterlaris";
 function App() {
 
   return (
-    <Router>
       <Routes>
         <Route path="/toko-keris" element={<Tokokeris />} />
         <Route path="/detail-produk" element={<Detailproduk />} />
         <Route path="/produk-terlaris" element={<ProdukTerlaris />} />
         <Route path="/detail-produk/:id" element={<Detailproduk />} />
       </Routes>
-    </Router>
   )
 }
 
