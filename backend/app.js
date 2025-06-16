c84d129323582590e978ed58dc26c567204eb950
Ari-fauzi <arifauzi441@gmail.com>
2025-06-16 14:52:30 +0700

    coba deploy ftp

--- backend/app.js
+++ backend/app.js
@@ -28,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 (async () => {
   await connectDB()
 })()
+console.log("hai")
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
