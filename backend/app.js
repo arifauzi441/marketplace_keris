0ac9f4de7c48216dca13c04a08361d3cfe28d35d
Ari-fauzi <arifauzi441@gmail.com>
2025-06-23 15:50:40 +0700

    change cors

--- backend/app.js
+++ backend/app.js
@@ -34,10 +34,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/images', cors({
+app.use('/images/:path', cors({
   origin: 'https://toko.kerissumenep.com',
   methods: ['GET']
-}), express.static(path.join(__dirname, 'public', 'images')));
+}), express.static(path.join(__dirname, 'public')));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
